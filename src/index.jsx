321717c87137b0e0c6074a8e21ee5715fa8d8df4
Jennifer Sandoval <san18962@uvg.edu.gt>
2022-06-18 15:58:27 -0600

    Change async version

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,5 @@ import StartApp from './components/startApp';
 import background from './backgroundMario.jpg';
 
 document.body.style.background = `url(${background})`;
-document.body.style.backgroundSize = '130% 100%';
+// document.body.style.backgroundSize = '50% 50%';
 ReactDOM.render(<StartApp />, document.getElementById('app'));
